938f03ded2cf06193d10fce28440576a27f6df48
christopherwitcher <christopher.witcher@live.com>
2022-01-06 11:14:57 -0800

    Update app.js with Azure Port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3030
+const port = process.env.port || 3030
 
 app.get('/', (req, res) => {
   res.send('Siblinge Node Refactor!')
